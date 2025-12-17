9243cd7b8de5773dbc0c6fe6baa2697c48bf69ee
Gobson995 <gugabadazanatta@gmail.com>
2025-12-17 12:48:45 -0300

    Correção para deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Scout Basquete",
-  description: "App de controle de estatísticas de basquete",
+  description: "App de estatísticas de basquete",
 };
 
 export default function RootLayout({
@@ -24,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50`}
-      >
+      <body className={inter.className}>
         {children}
       </body>
     </html>
